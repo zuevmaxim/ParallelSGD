8fe9697ecd75c3c1e2812396e2f9b921326b6682
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-12-15 13:22:41 +0300

    Generify tests

---
+++ src/test/kotlin/org/sgd/LogisticRegressionTest.kt
@@ -0,0 +1,194 @@
+package org.sgd.logisticRegression
+
+import benchmark.Profiler
+import kotlinx.smartbench.benchmark.*
+import kotlinx.smartbench.declarative.Operation
+import kotlinx.smartbench.graphic.PlotConfiguration
+import kotlinx.smartbench.graphic.Scaling
+import kotlinx.smartbench.graphic.ValueAxis
+import org.junit.jupiter.api.Test
+import org.sgd.*
+import java.util.concurrent.TimeUnit
+
+class LogisticRegressionTest {
+    private val DATASET = "covtype"
+
+    @Test
+    fun sequentialSolver() = runSequentialBenchmark(DATASET)
+
+    @Test
+    fun solverCompareWithProfiler() = runParallelBenchmarkWithProfiler(DATASET)
+
+    @Test
+    fun solverCompare() = runParallelBenchmark(DATASET)
+
+    @Test
+    fun run() {
+        val dataset = DATASET
+        val loss = models[dataset]!!()
+        val p = params[dataset]!!
+        val solver = ClusterParallelSGDSolver(p["learningRate"]!!, 128, p["stepDecay"]!!, 32)
+        val result = solver.solve(loss.first, loss.second, p["targetLoss"]!!)
+        println(loss.second.loss(result.w))
+    }
+
+    @Test
+    fun runAllDatasets() {
+        for (dataset in params.keys) {
+            runParallelBenchmark(dataset)
+        }
+    }
+}
+
+class RunRegressionTask(
+    val dataset: String,
+    val method: String,
+    val learningRate: Type,
+    val stepDecay: Type,
+    val workingThreads: Int,
+    val targetLoss: Type
+) : Benchmark() {
+    private val loss = models[dataset]!!()
+
+    @Operation
+    fun run(): Type {
+        val solver = when {
+            method == "simple" -> ParallelSGDSolver(learningRate, workingThreads, stepDecay)
+            method.startsWith(CLUSTER_METHOD_PREFIX) -> ClusterParallelSGDSolver(learningRate, workingThreads, stepDecay, method.substring(CLUSTER_METHOD_PREFIX.length).toInt())
+            else -> error("Unknown method $method")
+        }
+        val result = solver.solve(loss.first, loss.second, targetLoss)
+        return loss.second.loss(result.w)
+    }
+}
+
+class SequentialRegressionTask(
+    val dataset: String,
+    val learningRate: Type,
+    val stepDecay: Type,
+    val iterations: Int
+) : Benchmark() {
+    private val lossModel = models[dataset]!!()
+    var loss = BenchmarkCounter()
+    val counter = BenchmarkCounter()
+
+    @Operation
+    fun run(): Type {
+        val solver = SequentialSGDSolver(iterations, learningRate, stepDecay)
+        val result = solver.solve(lossModel.first, lossModel.second, ZERO)
+        return lossModel.second.loss(result.w).also {
+            val scale = 1e3.toLong()
+            loss.inc((it * scale).toLong())
+            counter.inc(scale)
+        }
+    }
+}
+
+private fun <T : Benchmark> PlotConfiguration<T>.commonConfigure(dataset: String, name: String, logarithmic: Boolean = true) {
+    filename("results/$dataset-$name.png")
+    if (logarithmic) {
+        xScaling(Scaling.LOGARITHMIC)
+    }
+    useErrorBars(true)
+}
+
+fun runSequentialBenchmark(dataset: String) {
+    val p = params[dataset]!!
+    runBenchmark<SequentialRegressionTask> {
+        param(SequentialRegressionTask::dataset, dataset)
+        param(SequentialRegressionTask::learningRate, p["learningRate"])
+        param(SequentialRegressionTask::stepDecay, p["stepDecay"])
+        param(SequentialRegressionTask::iterations, 1..10)
+        approximateBatchSize(p["batch"]!!.toInt())
+        measurementMode(MeasurementMode.AVERAGE_TIME, TimeUnit.SECONDS)
+        metric(AVERAGE_LOSS_METRIC) { loss.value / counter.value.toDouble() }
+        benchmarkMode(BenchmarkMode.INPLACE)
+    }.run {
+        plot(xParameter = SequentialRegressionTask::iterations) {
+            commonConfigure(dataset, "time")
+        }
+        plot(xParameter = SequentialRegressionTask::iterations) {
+            valueAxis(ValueAxis.CustomMetric(AVERAGE_LOSS_METRIC))
+            commonConfigure(dataset, AVERAGE_LOSS_METRIC, false)
+        }
+    }
+}
+
+private fun runParallelBenchmark(
+    dataset: String,
+    configureBenchmark: BenchmarkConfiguration<RunRegressionTask>.() -> Unit = {},
+    plotExtra: BenchmarkResults<RunRegressionTask>.() -> Unit = {}
+) {
+    val threadsPerCluster = logSequence(numaConfig.values.maxOf { it.size }).map { "$CLUSTER_METHOD_PREFIX$it" }
+    val threads = logSequence(Runtime.getRuntime().availableProcessors())
+    val p = params[dataset]!!
+    runBenchmark<RunRegressionTask> {
+        param(RunRegressionTask::dataset, dataset)
+        param(RunRegressionTask::method, threadsPerCluster)
+        param(RunRegressionTask::learningRate, p["learningRate"])
+        param(RunRegressionTask::stepDecay, p["stepDecay"])
+        param(RunRegressionTask::targetLoss, p["targetLoss"])
+        param(RunRegressionTask::workingThreads, threads)
+        approximateBatchSize(p["batch"]!!.toInt())
+        measurementMode(MeasurementMode.AVERAGE_TIME, TimeUnit.SECONDS)
+        configureBenchmark()
+    }.run {
+        plot(xParameter = RunRegressionTask::workingThreads) {
+            commonConfigure(dataset, "time")
+        }
+        plot(xParameter = RunRegressionTask::workingThreads) {
+            for (p: String in threadsPerCluster) {
+                val oneThreadTime = iterationResults.entries.single {
+                    val params = it.key.params
+                    params[RunRegressionTask::method.name]!!.param == p &&
+                        params[RunRegressionTask::workingThreads.name]!!.param == 1
+                }.value.resultValue(benchmarkConfiguration)
+                iterationResults.entries.filter {
+                    it.key.params[RunRegressionTask::method.name]!!.param == p
+                }.forEach {
+                    it.value.metrics[SPEEDUP_METRIC] = oneThreadTime / it.value.resultValue(benchmarkConfiguration)
+                }
+            }
+            commonConfigure(dataset, SPEEDUP_METRIC)
+            valueAxis(ValueAxis.CustomMetric(SPEEDUP_METRIC))
+        }
+        plotExtra()
+    }
+}
+
+fun runParallelBenchmarkWithProfiler(dataset: String) = runParallelBenchmark(dataset, {
+    attachProfiler(Profiler.LINUX_PEF_NORM_PROFILER)
+}) {
+    plot(xParameter = RunRegressionTask::workingThreads) {
+        commonConfigure(dataset, "LLC_store_misses")
+        valueAxis(ValueAxis.LLC_store_misses)
+    }
+    plot(xParameter = RunRegressionTask::workingThreads) {
+        commonConfigure(dataset, "LLC_stores")
+        valueAxis(ValueAxis.LLC_stores)
+    }
+    plot(xParameter = RunRegressionTask::workingThreads) {
+        commonConfigure(dataset, "LLC_loads")
+        valueAxis(ValueAxis.LLC_loads)
+    }
+    plot(xParameter = RunRegressionTask::workingThreads) {
+        commonConfigure(dataset, "LLC_load_misses")
+        valueAxis(ValueAxis.LLC_load_misses)
+    }
+    for (p: String in logSequence(numaConfig.values.maxOf { it.size }).map { "$CLUSTER_METHOD_PREFIX$it" }) {
+        iterationResults.entries.filter {
+            it.key.params[RunRegressionTask::method.name]!!.param == p
+        }.forEach {
+            it.value.metrics["store_misses, %"] = it.value.metrics["LLC-store-misses, #/op"]!!.toDouble() / it.value.metrics["LLC-stores, #/op"]!!.toDouble()
+            it.value.metrics["load_misses, %"] = it.value.metrics["LLC-load-misses, #/op"]!!.toDouble() / it.value.metrics["LLC-loads, #/op"]!!.toDouble()
+        }
+    }
+    plot(xParameter = RunRegressionTask::workingThreads) {
+        commonConfigure(dataset, "store_misses%")
+        valueAxis(ValueAxis.CustomMetric("store_misses, %"))
+    }
+    plot(xParameter = RunRegressionTask::workingThreads) {
+        commonConfigure(dataset, "load_misses%")
+        valueAxis(ValueAxis.CustomMetric("load_misses, %"))
+    }
+}
--- src/test/kotlin/org/sgd/common.kt
+++ src/test/kotlin/org/sgd/common.kt
@@ -12,28 +12,45 @@ val baseDir = File("../datasets").let {
     else File("/home/maksim.zuev/datasets")
 }
 
-val params = hashMapOf(
+val params: Map<String, Map<String, Type>> = hashMapOf(
+    "w8a" to hashMapOf(
+        "learningRate" to 0.26.toType(),
+        "stepDecay" to 1.0.toType(),
+        "targetLoss" to 0.02.toType(),
+        "batch" to 10.toType(),
+    ),
     "rcv1" to hashMapOf(
         "learningRate" to 0.5.toType(),
         "stepDecay" to 0.8.toType(),
-        "targetLoss" to 0.024.toType()
+        "targetLoss" to 0.024.toType(),
+        "batch" to 10.toType(),
     ),
     "webspam" to hashMapOf(
         "learningRate" to 0.2.toType(),
         "stepDecay" to 0.8.toType(),
-        "targetLoss" to 0.0756.toType()
+        "targetLoss" to 0.0756.toType(),
+        "batch" to 10.toType(),
     ),
     "news20" to hashMapOf(
         "learningRate" to 0.5.toType(),
         "stepDecay" to 0.8.toType(),
-        "targetLoss" to 0.1632.toType()
+        "targetLoss" to 0.1632.toType(),
+        "batch" to 10.toType(),
     ),
     "covtype" to hashMapOf(
         "learningRate" to 0.005.toType(),
         "stepDecay" to 0.85.toType(),
-        "targetLoss" to 0.2846.toType()
+        "targetLoss" to 0.2846.toType(),
+        "batch" to 10.toType(),
     ),
 )
+
+val models: Map<String, () -> Pair<Model, Model>> = hashMapOf(
+    "rcv1" to ::createBinaryModel,
+    "webspam" to ::createBinaryModel,
+    "news20" to ::createMulticlassModel,
+    "covtype" to ::createMulticlassModel
+).mapValues { { it.value(it.key) } }
 
 fun logSequence(maxValue: Int, step: Double = 2.0): List<Int> {
     var value = maxValue.toDouble()
@@ -44,6 +61,26 @@ fun logSequence(maxValue: Int, step: Double = 2.0): List<Int> {
     }
     return result.toList().sorted()
 }
+
+private val datasets = hashMapOf<String, Any>()
+
+private fun createBinaryModel(datasetName: String): Pair<Model, Model> {
+    val dataset = datasets.getOrPut(datasetName) { loadBinaryDataSet(File(baseDir, datasetName), File(baseDir, "$datasetName.t")) } as Pair<DataSet, DataSet>
+    val features = features(dataset.first, dataset.second)
+    val trainLoss = LogisticRegressionModel(dataset.first, features)
+    val testLoss = LogisticRegressionModel(dataset.second, features)
+    return trainLoss to testLoss
+}
+
+private fun createMulticlassModel(datasetName: String): Pair<Model, Model> {
+    val dataset = datasets.getOrPut(datasetName) { loadMulticlassDataSet(File(baseDir, datasetName), File(baseDir, "$datasetName.t")) } as Triple<DataSet, DataSet, Int>
+    val features = features(dataset.first, dataset.second)
+    val trainLoss = MulticlassLogisticRegressionModel(dataset.first, dataset.third, features)
+    val testLoss = MulticlassLogisticRegressionModel(dataset.second, dataset.third, features)
+    return trainLoss to testLoss
+}
+
+private fun features(train: DataSet, test: DataSet) = train.points.asSequence().plus(test.points).maxOf { it.indices.maxOrNull() ?: 0 }
 
 fun splitDataset(name: String) {
     val source = "../datasets/$name"
